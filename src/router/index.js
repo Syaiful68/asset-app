0d89d2364146e67bda6c3d28fceafdcd4d74daf8
Syaiful68 <syaifulSheva@gmail.com>
2025-06-13 16:08:06 +0700

    create aramada detail

--- src/router/index.js
+++ src/router/index.js
@@ -1,100 +1,104 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory } from "vue-router";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/asset',
-      name: 'asset',
-      children:[
+      path: "/asset",
+      name: "asset",
+      children: [
         {
-          path:'',
-          name:'asset.index',
-          component: () => import('../views/Asset/index.vue'),
+          path: "",
+          name: "asset.index",
+          component: () => import("../views/Asset/index.vue"),
         },
         {
-          path:'create',
-          name:'asset.create',
-          component: () => import('../views/Asset/create.vue'),
+          path: "create",
+          name: "asset.create",
+          component: () => import("../views/Asset/create.vue"),
         },
         {
-          path:':id/detail',
-          name:'asset.detail',
-          component: () => import('../views/Asset/detail.vue'),
+          path: ":id/detail",
+          name: "asset.detail",
+          component: () => import("../views/Asset/detail.vue"),
         },
-      ]
+      ],
     },
     {
-      path: '/building',
-      name: 'building',
-      children:[
+      path: "/building",
+      name: "building",
+      children: [
         {
-          path:'',
-          name:'building.index',
-          component: () => import('../views/Building/index.vue'),
+          path: "",
+          name: "building.index",
+          component: () => import("../views/Building/index.vue"),
         },
         {
-          path:'create',
-          name:'building.create',
-          component: () => import('../views/Building/create.vue'),
+          path: "create",
+          name: "building.create",
+          component: () => import("../views/Building/create.vue"),
         },
         {
-          path:':id/detail',
-          name:'building.detail',
-          component: () => import('../views/Building/detail.vue'),
+          path: ":id/detail",
+          name: "building.detail",
+          component: () => import("../views/Building/detail.vue"),
         },
-      ]
+      ],
     },
     {
-      path: '/armada',
-      name: 'armada',
-      children:[
+      path: "/armada",
+      name: "armada",
+      children: [
         {
-          path:'',
-          name:'armada.index',
-          component: () => import('../views/Armada/index.vue'),
+          path: "",
+          name: "armada.index",
+          component: () => import("../views/Armada/index.vue"),
         },
         {
-          path:':id/detail',
-          name:'armada.detail',
-          component: () => import('../views/Armada/detail.vue'),
+          path: "create",
+          name: "armada.create",
+          component: () => import("../views/Armada/create.vue"),
         },
-      ]
+        {
+          path: ":id/detail",
+          name: "armada.detail",
+          component: () => import("../views/Armada/detail.vue"),
+        },
+      ],
     },
     {
-      path: '/vendor',
-      name: 'vendor',
-      children:[
+      path: "/vendor",
+      name: "vendor",
+      children: [
         {
-          path:'',
-          name:'vendor.index',
-          component: () => import('../views/Vendor/index.vue'),
+          path: "",
+          name: "vendor.index",
+          component: () => import("../views/Vendor/index.vue"),
         },
         {
-          path:':id/detail',
-          name:'vendor.detail',
-          component: () => import('../views/Vendor/detail.vue'),
+          path: ":id/detail",
+          name: "vendor.detail",
+          component: () => import("../views/Vendor/detail.vue"),
         },
-      ]
+      ],
     },
     {
-      path: '/report',
-      name: 'report',
-      children:[
+      path: "/report",
+      name: "report",
+      children: [
         {
-          path:'repair',
-          name:'report.repair',
-          component: () => import('../views/Report/repair.vue'),
+          path: "repair",
+          name: "report.repair",
+          component: () => import("../views/Report/repair.vue"),
         },
         {
-          path:'asset',
-          name:'report.asset',
-          component: () => import('../views/Report/asset.vue'),
+          path: "asset",
+          name: "report.asset",
+          component: () => import("../views/Report/asset.vue"),
         },
-      ]
+      ],
     },
-
   ],
-})
+});
 
-export default router
+export default router;
--- src/views/Armada/create.vue
+++ src/views/Armada/create.vue
@@ -7,6 +7,21 @@ const headers = [
     path: [{ label: "Armada", link: "", name: "Armada" }],
   },
 ];
+const compenies = [
+  { name: "CV. Aufa", id: 1 },
+  { name: "PT. NJA", id: 2 },
+];
+const typeCar = [
+  { name: "Car", id: 1 },
+  { name: "Truck", id: 2 },
+  { name: "Van", id: 3 },
+];
+const Fuels = [
+  { name: "Pertalite", id: 1 },
+  { name: "Bio Solar", id: 2 },
+  { name: "Petamax", id: 3 },
+  { name: "Dexlite", id: 4 },
+];
 </script>
 
 <template>
@@ -15,8 +30,70 @@ const headers = [
   <div class="page-body">
     <div class="container-xl">
       <div class="row row-deck row-cards">
-        <div class="col-12">
-          <TableView :header="tableHeader" :data="datas" />
+        <div class="col-6">
+          <div class="card">
+            <div class="card-body">
+              <div class="mb-3">
+                <label class="form-label">Compeny</label>
+                <select class="form-control">
+                  <option value="">Choise</option>
+                  <option v-for="(item, index) in compenies" :value="item.id">
+                    {{ item.name }}
+                  </option>
+                </select>
+              </div>
+              <div class="mb-3">
+                <label class="form-label">Plat Number</label>
+                <input type="text" class="form-control" />
+              </div>
+              <div class="mb-3">
+                <label class="form-label">Contract</label>
+                <input type="text" class="form-control" />
+              </div>
+              <div class="mb-3">
+                <label class="form-label">Rent</label>
+                <input type="text" class="form-control" />
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-6">
+          <div class="card">
+            <div class="card-body">
+              <div class="mb-3">
+                <label class="form-label">Vehicle</label>
+                <input type="text" class="form-control" />
+              </div>
+              <div class="mb-3">
+                <label class="form-label">Type</label>
+                <select class="form-control">
+                  <option value="">Choise</option>
+                  <option v-for="(item, index) in typeCar" :value="item.id">
+                    {{ item.name }}
+                  </option>
+                </select>
+              </div>
+              <div class="mb-3">
+                <label class="form-label">Fuels</label>
+                <select class="form-control">
+                  <option value="">Choise</option>
+                  <option v-for="(item, index) in Fuels" :value="item.id">
+                    {{ item.name }}
+                  </option>
+                </select>
+              </div>
+
+              <div class="mb-3">
+                <label class="form-label">Description</label>
+                <input type="text" class="form-control" />
+              </div>
+            </div>
+            <div class="card-footer">
+              <div class="d-flex justify-content-end">
+                <button type="submit" class="btn btn-success">Submit</button>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
